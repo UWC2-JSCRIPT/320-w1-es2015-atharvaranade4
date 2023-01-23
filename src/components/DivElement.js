884ff8cd57bc91b1054b0349e744c40acda8a97c
Atharva Ranade <atharvaranade4@gmail.com>
2023-01-22 17:04:58 -0800

    first pass

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,23 @@
 // Import HTMLElement here
+import HTMLElement from './HTMLElement';
 
 // Define class here
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super();
+    this.content = content;
+  }
+
+  render() {
+    super.render();
+  }
+}
+
+// Usage Tests
+const andIThinkToMyself = new DivElement('What a wonderful world');
+
+console.log(andIThinkToMyself.render());
+// prints "<div>What a wonderful world</div>"
 
 // Export class here
+export default { DivElement };
